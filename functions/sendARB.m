ddedd910c8207f6851affbc4e478f7cc305a3b86
PCote-Tech <PCote@mtech.edu>
2019-05-17 16:01:57 -0600

    Documentation and code clean up on sendArb

--- functions/sendARB.m
+++ functions/sendARB.m
@@ -1,10 +1,41 @@
-function sendARB(x,Vpp,Fs,filtFlag,instrumentType,intrumentAddress)
-% sendARB.m
+function sendARB(x,Vpp,Fsamp,chnlFilt,instrumentType,intrumentAddress)
+%% sendARB.m
+%   Send matlab generated waveforms to an arbitrary waveform generator.
+%   Currently, this function has only been tested with the Keysight 33500
+%   waveform generator. IVI drivers must be installed, see
+%   documentation for more help.
+%
+% INPUTS:
+%       x                   waveform column vectors
+%       Vpp                 Peak-Peak Voltage of Signal
+%       Fsamp               Output Sample rates
+%       chnlFilt            Channel Filter (off,Normal,Step)->(0,1,2)
+%       instrumentType      VISA Instrument Type
+%                           1       - NI
+%                           2       - Agilent
+%                           'xxxx'  - User Specified
+%                           Default - KEYSIGHT
+%       intrumentAddress    VISA Instrument Address
+%
+%
+% % Two Channel Mode %
+% Input for two channels: x = [xCh1, xCh2] where xCh1 and xCh2 are column vectors
+% If only single values are supplied for Vpp,Fsamp, or chnlFilt, that value
+% will be used for both waveforms.
+%
+% % Only Channel 2 Mode %
+% If only one waveform is supplied the default is to send it to Channel 1
+% To load a single waveform into Channel 2, the Vpp input must have two
+% values.
 
+% Adapted from:
+%       arbTo33500_nchannel.m   by Tyler Holliday
+%       arbTo33500.m            by Neil Forcier
+%       AgArbTrans_V2.m         by Salaheddin Hosseinzadeh
 
 %% Input Check
-
 % Check Signal Size
+% TODO: Verify signal input structure is one or two column vectors
 xSize = size(x);
 if xSize(2) < 2
     Nx = 1; % Only one signal to send
@@ -15,11 +46,11 @@ else
     if length(Vpp)==1
         Vpp = [Vpp Vpp];
     end
-    if length(Fs)==1
-        Fs = [Fs Fs];
+    if length(Fsamp)==1
+        Fsamp = [Fsamp Fsamp];
     end
-    if length(filtFlag)==1
-        filtFlag = [filtFlag filtFlag];
+    if length(chnlFilt)==1
+        chnlFilt = [chnlFilt chnlFilt];
     end
 end
 
@@ -84,7 +115,6 @@ h = waitbar(0,mes);
 % Reset instrument
 fprintf(visaObj,'*RST');
 
-
 %% Send waveform(s)
 % For each waveform
 for n = 1:Nx
@@ -110,39 +140,39 @@ for n = 1:Nx
     % Update waitbar
     waitbar(0.1,h,mes);
     
-    % configure memory
+    % Configure Memory
     fprintf(visaObj,[chnl,':DATA:VOLatile:CLEar']);     % Clear volatile memory
     fprintf(visaObj,'FORM:BORD SWAP');                  % configure the box to correctly accept the binary arb points
     arbBytes=num2str(length(signal)*4);                   % # of bytes
     
-    % generate header
+    % Generate Header
     header = [chnl,':DATA:ARBitrary ARB',num2str(chnlNdx),', #', num2str(length(arbBytes)), arbBytes];
     binblockBytes = typecast(signal,'uint8');             % convert datapoints to binary before sending
     fwrite(visaObj,[header binblockBytes],'uint8');     % combine header and datapoints then send to instrument
     fprintf(visaObj,'*WAI');                            % Make sure no other commands are exectued until arb is done downloadin
     
-    % update waitbar
+    % Update waitbar
     waitbar(0.8/Nx,h,mes);
     
-    % set ARB name
+    % Set ARB name
     command = [chnl,':FUNCtion:ARBitrary ARB', num2str(chnlNdx)];    % create name command
     fprintf(visaObj,command);                           % set current arb waveform to defined arb testrise
 
     % update waitbar
     waitbar(0.9/Nx,h,mes);
     
-    % set sample rate
-    command = [chnl,':FUNCtion:ARB:SRATe +' num2str(Fs(n))]; % create sample rate command
+    % Set sample rate
+    command = [chnl,':FUNCtion:ARB:SRATe +' num2str(Fsamp(n))]; % create sample rate command
     fprintf(visaObj,command);
     
-    % select ARB mode
+    % Select ARB mode
     fprintf(visaObj,[chnl,':FUNCtion ARB']);        % turn on arb function
     
-    % set amplitude, Vpp
+    % Set amplitude, Vpp
     command = [chnl,':VOLT ' num2str(Vpp(n))];      % create amplitude command
     fprintf(visaObj,command);                       % send amplitude command
     
-    % set offset
+    % Set offset
     fprintf(visaObj,[chnl,':VOLT:OFFSET 0']);      % set offset to 0 V
     
     % confirm upload
@@ -161,8 +191,9 @@ for n = 1:Nx
         fprintf (errorcheck)
     end
     
-    switch filtFlag(n)
-        % set filter type
+    % set filter type
+    switch chnlFilt(n)
+
         case 0
             fprintf(visaObj,[chnl,':FUNCtion:ARB:FILT OFF']);	% disable filter
         case 1
@@ -172,19 +203,18 @@ for n = 1:Nx
         otherwise
     end
 
-    % enable CHannel
+    % Enable CHannel
     fprintf(visaObj,['OUTPUT',num2str(chnlNdx),' ON']);
     
-
-    % Delay between setting up channels
-    % update waitbar
+    % Update waitbar status
     waitbar(0.95/Nx,h,mes);
+    
+    % Wait for device
     fprintf(visaObj,'*WAI');
 
 end
-%%
-    
-%% If multipleSync arbs
+
+%% Sync arbs
 fprintf(visaObj,'FUNC:ARB:SYNC');
 
 %% Clean Up
